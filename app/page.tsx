d1d0d9ae1e29fec94fe9002320ccd7c4301c347d
Muskaan-git9 <muskanpatel505@gmail.com>
2025-03-18 17:57:57 +0530

    changes in sidebar

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@
 
 import { useState } from "react";
 import AmenitiesList from "@/components/ui/AmenitiesList";
-import Sidebar from "@/components/SideBar";
+import SideBar from "@/components/ui/SideBar";
 
 export default function Layout() {
   const [isSidebarOpen, setSidebarOpen] = useState(false);
@@ -13,7 +13,7 @@ export default function Layout() {
         className={`${isSidebarOpen ? "w-64" : "w-0 md:w-64 overflow-hidden"
           } fixed md:relative top-0 left-0 h-full bg-white z-50 shadow-lg transition-all duration-300`}
       >
-        <Sidebar isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <SideBar isSidebarOpen={isSidebarOpen} setSidebarOpen={setSidebarOpen} />
       </div>
       <div className="flex-1 p-6 bg-gray-100 transition-all duration-300">
         <div className="flex flex-col sm:flex-row sm:justify-end sm:ml-0 ml-6">
